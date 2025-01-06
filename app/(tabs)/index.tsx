d29bf9de355537b997911072b7fd14a20aefcd12
winstontsui <ny1winston@gmail.com>
2025-01-06 17:28:51 -0500

    First commit, crashes expo app

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen(): React.JSX.Element {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
+  );
+}
+
+
+
