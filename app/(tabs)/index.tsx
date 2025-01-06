e51f252b982c417d37a9ac43d90fa8aab41260de
winstontsui <ny1winston@gmail.com>
2025-01-06 17:51:55 -0500

    Fixed crash by downgrading tailwind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
-import { View, Text, StatusBar } from 'react-native';
+import { Text, StatusBar } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen(): React.JSX.Element {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* <StatusBar style="auto" /> */}
+      <Text className={"text-red-500"}> Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
